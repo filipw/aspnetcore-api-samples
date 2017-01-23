00b94874ead98f5bb62a228a51516b9bec8e99c9
filipw <wojcieszyn@gmail.com>
2017-01-23 19:52:22 +0100

    added mvc core without auth

---
+++ _02 mvc core api/src/MvcCoreApi/Controllers/ContactsController.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreApi.Models;
+
+namespace MvcCoreApi.Controllers
+{
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactRepository _repository;
+
+        public ContactsController(IContactRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _repository.GetAll());
+        }
+
+        [HttpGet("{id}", Name = "GetContactById")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var contact = await _repository.Get(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Post([FromBody]Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                var newId = await _repository.Add(contact);
+                return CreatedAtRoute("GetContactById", new { id = newId }, contact);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Contact contact)
+        {
+            contact.ContactId = id;
+            await _repository.Update(contact);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _repository.Get(id);
+            if (deleted == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ _02 mvc core api/src/MvcCoreApi/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MvcCoreApi.Models;
+
+namespace MvcCoreApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IContactRepository, InMemoryContactRepository>();
+            services.AddMvcCore().
+                AddAuthorization().
+                AddDataAnnotations().
+                AddJsonFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            app.UseMvc();
+        }
+    }
+}
