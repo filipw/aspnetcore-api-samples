87a29e17be1d4825dbb0140cd0caa3e46902893b
filipw <wojcieszyn@gmail.com>
2017-09-28 16:33:48 +0200

    more updated to .net core 2.0

--- 03 MVC Core API/MvcCoreApi/Program.cs
+++ 03 MVC Core API/MvcCoreApi/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using MvcCoreApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MvcCoreApi
 {
@@ -10,8 +15,29 @@ namespace MvcCoreApi
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, l) =>
+                {
+                    l.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    l.AddConsole();
+                })
                 .UseIISIntegration()
-                .UseStartup<Startup>()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IContactRepository, InMemoryContactRepository>();
+                    services.
+                        AddMvcCore().
+                        AddDataAnnotations().
+                        AddJsonFormatters();
+                })
+                .Configure(app =>
+                {
+                    app.UseMvc();
+                })
                 .Build();
 
             host.Run();
